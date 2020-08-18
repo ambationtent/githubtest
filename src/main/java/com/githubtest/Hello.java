ea20418f83ddb03b9944ad925a046746e460064b
wei <weizx9827@163.com>
2020-08-18 15:35:18 +0800

    githubtest

---
+++ src/main/java/com/githubtest/Hello.java
@@ -0,0 +1,14 @@
+package com.githubtest;
+
+/**
+ * calss Hello
+ * 测试上传github
+ * @author WEIZHIXIN
+ * @date 2020/08/18
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
